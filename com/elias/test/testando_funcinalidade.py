f538039c4864fee9a3ff213dd0ba5a43517b2f9a
elias <elias.oliveira@aluno.faculdadeimpacta.com.br>
2020-10-11 12:47:44 -0300

    prints e funcoes

---
+++ com/elias/test/testando_funcinalidade.py
@@ -0,0 +1,17 @@
+# exercicio 04
+
+def func(x):
+    return x + 1
+
+def test_answer():
+    assert func(3) == 4
+
+def test_negativo():
+    assert func(-2) == -1
+
+def test_zero():
+    assert func(0) == 1
+
+def test_funcao():
+    assert func(1) == 2
+
